d4e3b01d3b41fb0feecc9345fc79970a1731b66a
Joy <jmail840619@gmail.com>
2018-04-17 21:06:10 +0800

    Fix bug in preprocessing

--- script/preprocessing.m
+++ script/preprocessing.m
@@ -1,40 +1,42 @@
 
 currentFolder = pwd;
 if ~exist('data/Altered','dir')
+    % make directories for new folders
     mkdir('data/Altered');
     mkdir('data/Altered/NG');
     mkdir('data/Altered/PASS');
-end
-dataPath = fullfile(currentFolder,'data');
-% input 
-NGdataPath = fullfile(dataPath,'Original','NG');
-PASSdataPath = fullfile(dataPath,'Original','PASS');
-% output
-NGOutputPath = fullfile(dataPath,'Altered','NG');
-PASSOutputPath = fullfile(dataPath, 'Altered','PASS');
 
-% resize images
-% NGs = dir(NGdataPath);
-% for i=3:numel(NGs)
-%    name =  NGs(i).name;
-%    img = imread(fullfile(NGdataPath,name));
-%    if ~isequal(size(img), [70,70])
-%        temp_img = imresize(img, [70,70]);
-%    else
-%        temp_img = img;
-%    end
-%    imwrite(temp_img, fullfile(NGOutputPath, name), 'bmp');
-% end
+    dataPath = fullfile(currentFolder,'data');
+    % input 
+    NGdataPath = fullfile(dataPath,'Original','NG');
+    PASSdataPath = fullfile(dataPath,'Original','PASS');
+    % output
+    NGOutputPath = fullfile(dataPath,'Altered','NG');
+    PASSOutputPath = fullfile(dataPath, 'Altered','PASS');
 
-PASSs =  dir(PASSdataPath);
-for i=3:numel(PASSs)
-   name =  PASSs(i).name;
-   img = imread(fullfile(PASSdataPath,name));
-   if ~isequal(size(img), [70,70])
-       temp_img = imresize(img, [70,70]);
-   else
-       temp_img = img;
-   end
-   imwrite(temp_img, fullfile(PASSOutputPath, name));
-    
+    % resize images
+    NGs = dir(NGdataPath);
+    for i=3:numel(NGs)
+       name =  NGs(i).name;
+       img = imread(fullfile(NGdataPath,name));
+       if ~isequal(size(img), [70,70])
+           temp_img = imresize(img, [70,70]);
+       else
+           temp_img = img;
+       end
+       imwrite(temp_img, fullfile(NGOutputPath, name));
+    end
+
+    PASSs =  dir(PASSdataPath);
+    for i=3:numel(PASSs)
+       name =  PASSs(i).name;
+       img = imread(fullfile(PASSdataPath,name));
+       if ~isequal(size(img), [70,70])
+           temp_img = imresize(img, [70,70]);
+       else
+           temp_img = img;
+       end
+       imwrite(temp_img, fullfile(PASSOutputPath, name));
+
+    end
 end
